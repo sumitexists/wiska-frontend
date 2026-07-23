96cc78c0c43584fc9d6cdb97cbd0d085c644d887
Sumit Singh <sumitsingh290707@gmail.com>
2026-07-23 14:40:36 +0530

    changed h-dvh to h-full

--- src/pages/VerifiedProfilePage.jsx
+++ src/pages/VerifiedProfilePage.jsx
@@ -11,8 +11,8 @@ export default function VerifiedProfilePage() {
     }
     return(
 
-        <div className="w-full h-dvh grid grid-cols-[5fr_10fr]">
-            <div className="flex flex-col w-full h-full bg-zinc-800 border-r border-zinc-700">
+        <div className="w-full h-full grid grid-cols-[5fr_10fr]">
+            <div className="flex flex-col w-full h-screen bg-zinc-800 border-r border-zinc-700">
                 <div className=" py-6 px-4 bg-zinc-800 text-white flex justify-between items-center">
                     <span className="text-3xl font-bold">Contacts</span>
                     <button className="bg-white text-black px-4 py-2 font-mono tracking-tighter transition-transform duration-300 hover:scale-103 hover:shadow-[3px_3px_7px] shadow-zinc-600" onClick={handleOnClick }>
